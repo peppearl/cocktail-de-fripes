5f1c06ba8e84938adc18c6a0898ab3778110eb57
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2021-04-08 22:02:54 +0200

    Reducing BlogScreen size by creating new component

--- app/screens/BlogScreen.js
+++ app/screens/BlogScreen.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {StyleSheet, ScrollView, View} from 'react-native';
-import newsApi from '../api/newsApi';
+import useNews from '../hooks/useNews';
 import FeaturedNews from "../blog/FeaturedNews";
 import BreakingNews from "../blog/BreakingNews";
 import TechNews from "../blog/TechNews";
@@ -10,34 +10,13 @@ import PoliticalNews from "../blog/PoliticalNews";
 
 export default function BlogScreen() {
 
-    const [featuredNews, setFeaturedNews] = useState({});
-    const [breakingNews, setBreakingNews] = useState([]);
-    const [politicalNews, setPoliticalNews] = useState([]);
-    const [techNews, setTechNews] = useState([]);
-    const [entertainmentNews, setEntertainmentNews] = useState([]);
-    const qty = 5;
-
-    const filterFeatured = (data) => {
-        return [...data].filter(item => item.featured === 'on').reverse()[0];
-    }
-
-    const filterByCategory = (data, category)  => {
-        return [...data].filter(item => item.category === category).reverse().splice(0, qty)
-    }
-
-    const filterMultipleNews = async () => {
-        const allNews = await newsApi.getAll();
-
-        setFeaturedNews(filterFeatured(allNews));
-        setBreakingNews(filterByCategory(allNews, 'breaking-news'));
-        setPoliticalNews(filterByCategory(allNews, 'political'));
-        setEntertainmentNews(filterByCategory(allNews, 'entertainment'));
-        setTechNews(filterByCategory(allNews, 'tech'));
-    }
-
-    useEffect(() => {
-        filterMultipleNews()
-    }, []);
+    const [
+        featuredNews,
+        politicalNews,
+        entertainmentNews,
+        techNews,
+        breakingNews
+    ] = useNews()
 
     return (
 
